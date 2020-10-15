d2b174f222b1ddacfd93cd92af535ba0b7a30d5a
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-10-15 07:54:51 -0700

    Practical demo of middlewares

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { body, validationResult } = require("express-validator");
 
 const PORT = 3000;
 
@@ -8,15 +9,13 @@ const PORT = 3000;
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 // Setting view engine
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 // Setting views directory
-app.set('views', path.join(__dirname, "templates"))
+app.set("views", path.join(__dirname, "templates"));
 
-// Setting routes 
-app.use('/users', require('./routes/users'))
-app.use('/students', require('./routes/students'))
-
-
+// Setting routes
+app.use("/users", require("./routes/users"));
+app.use("/students", require("./routes/students"));
 
 // app.get("/", (req, res) => {
 //   res.json({ name: "John", age: 34 });
@@ -31,8 +30,6 @@ app.use('/students', require('./routes/students'))
 //   res.send(req.body);
 // });
 
-
-
 // app.get('/test', (req, res) => {
 //   res.render('test', {
 //     title: "Custom Title",
@@ -40,15 +37,30 @@ app.use('/students', require('./routes/students'))
 //   });
 // })
 
+// Creating middleware
+app.use((req, res, next) => {
+  console.log("Current time", Date.now());
+  next();
+});
 
 app.get("/demo-form", (req, res) => {
-  res.render('form')
-})
+  res.render("form");
+});
 
-app.post("/demo-form", (req, res) => {
-  console.log(req.body);
-  res.json(req.body)
-})
+app.post(
+  "/demo-form",
+  [body("title").not().isEmpty().withMessage("The title field is required")],
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    console.log(req.body);
+    res.json({ data: req.body });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
