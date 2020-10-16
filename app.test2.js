618952c31be26fc2e74ee497099686f285f412ae
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-10-16 07:55:28 -0700

    Populated pug view with validation errors

--- app.test2.js
+++ app.test2.js
@@ -13,6 +13,36 @@ app.set("view engine", "pug");
 // Setting views directory
 app.set("views", path.join(__dirname, "templates"));
 
+// Setting routes
+app.use("/users", require("./routes/users"));
+app.use("/students", require("./routes/students"));
+
+// app.get("/", (req, res) => {
+//   res.json({ name: "John", age: 34 });
+// });
+
+// app.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "pages", "about.html"));
+// });
+
+// app.post("/users", (req, res) => {
+//   req.body.name = req.body.name.toUpperCase();
+//   res.send(req.body);
+// });
+
+// app.get('/test', (req, res) => {
+//   res.render('test', {
+//     title: "Custom Title",
+//     message: "Custom Message",
+//   });
+// })
+
+// Creating middleware
+app.use((req, res, next) => {
+  console.log("Current time", Date.now());
+  next();
+});
+
 app.get("/demo-form", (req, res) => {
   res.render("form");
 });
@@ -20,12 +50,11 @@ app.get("/demo-form", (req, res) => {
 app.post(
   "/demo-form",
   [body("title").not().isEmpty().withMessage("The title field is required")],
-  [body("email").isEmail().withMessage("Please provide a valid email address")],
   (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.render('form', { errors: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     console.log(req.body);
