1a5cf0a271d5b08305c2513e65a459994e9dac22
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-10-26 08:40:30 -0700

    Added aggregates file

---
+++ mysql/aggregates.sql
@@ -0,0 +1,11 @@
+SELECT SUM(`rice`) AS rice_sum FROM `sells`
+
+SELECT SUM(`rice`) AS rice_sum, SUM(`sugar`) AS sugar_sum FROM `sells`
+
+SELECT COUNT(`rice`) FROM `sells`
+
+SELECT AVG(`rice`) AS average_rice FROM `sells`
+
+SELECT MIN(`rice`) AS min_rice FROM `sells`
+
+SELECT MAX(`rice`) AS max_rice FROM `sells`
