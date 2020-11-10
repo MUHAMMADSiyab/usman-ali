08f5b6563a10b10bbaf907703e37d628e48907f6
MUHAMMAD Siyab <siyab.dev@gmail.com>
2020-11-09 20:15:39 -0800

    Update routes file. Discussed route parameters

--- node-mysql-demo/routes/users.js
+++ node-mysql-demo/routes/users.js
@@ -20,4 +20,21 @@ router.get("/all", (req, res) => {
   );
 });
 
+router.get("/:id/edit", (req, res) => {
+  
+  const q = `SELECT * FROM users WHERE id = ${req.params.id}`;
+
+  db.query(q, (err, data) => {
+    if (data.length === 0) {
+      return res.send("<h1>404 - Not Found</h1>")
+    }
+
+    // res.json(data[0])
+
+    res.render('user-edit', {
+      user: data[0],
+    })
+  })
+});
+
 module.exports = router;
